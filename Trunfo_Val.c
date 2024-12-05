cf6ee93858e446d05fbf93d9c1486978eaa5dff6
Mayaomoto <mayaomoto@gmail.com>
2024-12-05 16:39:43 -0300

    Esqueleto do Trunfo

---
+++ Trunfo_Val.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct 
+{
+    char Nome [31];
+    char Letra;
+    int Numero_do_agente;
+    bool;
+    int poder;
+    int altura;
+    int idade;
+    int peso;
+    
+}Agente;
+
+int main (){
+
+    Agente agentes [33];
+
+
+    return 0;
+}
